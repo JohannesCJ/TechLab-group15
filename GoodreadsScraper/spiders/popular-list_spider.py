28e1d9b7c2e65732ce4ba16047e6ed1d98abc57b
J.Heyn <j.heyn@physik.uni-muenchen.de>
2020-05-29 00:20:00 +0200

    Fetching more data
    Downloading all available entries

--- GoodreadsScraper/spiders/popular-list_spider.py
+++ GoodreadsScraper/spiders/popular-list_spider.py
@@ -6,6 +6,10 @@ Created on Mon May 25 20:35:34 2020
 """
 
 """Spider to extract URL's of books from a Listopia list on Goodreads"""
+"""Run following command in bash: 
+Johannes Heyn@JohannesSurface MINGW64 /d/OneDrive - Universität Münster/Studium/TechLabs-AI/TechLab-group15 (master)
+$ scrapy crawl --logfile=scrapy.log -s OUTPUT_FILE_SUFFIX="book_popularByDate2" popular-list
+"""
 
 import scrapy
 
@@ -26,17 +30,27 @@ class ListSpider(scrapy.Spider):
     def __init__(self):
         super().__init__()
         self.book_spider = BookSpider()
-        months_2019 = ["October", "November", "December"]
-        months_2020 = ["January", "February", "March", "April", "May"]
+        #months_2019 = ["October", "November", "December"]
+        #months_2020 = ["January", "February", "March", "April", "May"]
+        months_2019 = ["January", "February", "March", "April", "May", "June",
+                       "July", "August", "September"] #missing last time
+        months_2021 = ["January", "February", "March", "April", "May", "June",
+                       "July", "August", "September", "October", "November", 
+                       "December"] #expected publications
 
         self.start_urls = []
+        # for month in months_2020:
+        #     year = 2020
+        #     list_url = self.goodreads_list_url.format(year, month)
+        #     self.start_urls.append(list_url)
+            
         for month in months_2019:
             year = 2019
             list_url = self.goodreads_list_url.format(year, month)
             self.start_urls.append(list_url)
-            
-        for month in months_2020:
-            year = 2020
+
+        for month in months_2021:
+            year = 2021
             list_url = self.goodreads_list_url.format(year, month)
             self.start_urls.append(list_url)
 
