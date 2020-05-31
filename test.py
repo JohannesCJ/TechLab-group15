0d19e1d0370b1221ba5a3fc5578679cf2d244565
HRK777 <henrik.f.kram29@hotmail.com>
2020-05-31 22:06:21 +0200

    Update test.py

--- test.py
+++ test.py
@@ -9,18 +9,36 @@ import datetime
 from ast import literal_eval
 from collections import Counter
 
-#get the counts of all genres in the genre column
-def gen_dicker(genre_col):
-    dic = {}
-    for value in genre_col:
-        try:
-            liste = literal_eval(value)
-            for genre in liste:
-                if genre not in dic:
-                    dic[genre] = 1
-                else:
-                    dic[genre] += 1
-        except ValueError:
-            continue
+def post_timer(age, publish_date):
+    """
+    takes the age & publish_date of a book to output a random post date. The post_date is sampled randomly (&uniformly) from the range (start = 0, end = age)
+    input: num_reviews, publish_date
+    returns: post_date
+    """
+    #draw random day difference from age
+    x = np.random.randint(0, age + 1, 1)
 
-    return dic
+    #define timedelta
+    offset = pd.Timedelta(x[0], 'D')
+
+    #define post_date
+    post_date = publish_date + offset
+
+    return post_date
+
+def post_applier(row):
+    """
+    applies the post_timer function to a whole data frame to output new post_date column. The column automatically has the right datetime format
+
+    input: data frame row
+    output: new post_date column
+    """
+    #get input variables
+    age = row.age 
+    publish_date = row.publish_date
+
+    return post_timer(age, publish_date)
+
+
+
+
